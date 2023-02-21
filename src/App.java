bac5fd41dea2f1ddf86c4f0ab8daa1c9a9a8ea8b
Kevin Diego <kevindiegodasilvasousa@gmail.com>
2023-02-21 09:27:30 -0300

    feat: Criando os submenu de opções

--- src/App.java
+++ src/App.java
@@ -2,12 +2,39 @@ import javax.swing.JOptionPane;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Object[] itens = { "currency converter", "temperature converter", "distance converter"};
+        //Menu de seleção dos conversores//
+        Object[] itens = { "currency converter", "temperature converter", "distance converter" };
         Object selectedValue = JOptionPane.showInputDialog(null,
-          "Please, select a item", "Opçao",
-              JOptionPane.INFORMATION_MESSAGE, null,
-                  itens, itens [0]);
+                "Please, select a item", "Opçao",
+                JOptionPane.INFORMATION_MESSAGE, null,
+                itens, itens[0]);
 
-        System.out.println(selectedValue);
+        if (selectedValue == "currency converter") {
+            //menu de selecao para as moedas//
+            Object[] opcoes = { "Dolar", "Libra", "Euro", "Real", "Pesos" };
+            Object coins = JOptionPane.showInputDialog(null,
+                    "Please, select a item", "Opçao",
+                    JOptionPane.INFORMATION_MESSAGE, null,
+                    opcoes, opcoes[0]);
+            System.out.println(coins);
+
+        } else if (selectedValue == "temperature converter") {
+            //menu de selecao para temperaturas//
+            Object[] opcoes = { "Celsius", "Farenheit", "Kelvin"};
+            Object temp = JOptionPane.showInputDialog(null,
+                    "Please, select a item", "Opçao",
+                    JOptionPane.INFORMATION_MESSAGE, null,
+                    opcoes, opcoes[0]);
+            System.out.println(temp);
+
+        } else if (selectedValue == "distance converter") {
+            //menu de selecao para as distancias//
+            Object[] opcoes = { "Decâmetro", "Hectômetro", "Quilômetro", "Decímetro", "Centímetro", "Milímetro"};
+            Object temp = JOptionPane.showInputDialog(null,
+                    "Please, select a item", "Opçao",
+                    JOptionPane.INFORMATION_MESSAGE, null,
+                    opcoes, opcoes[0]);
+            System.out.println(temp);
+        }
     }
 }
