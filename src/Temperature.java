aac594b883adf53976e4430d576b8196d53f0c02
Kevin Diego <55944691+KevinDik@users.noreply.github.com>
2023-02-23 19:44:52 +0000

    refatorando a logica e escrevendo o pseudocodigo

---
+++ src/Temperature.java
@@ -0,0 +1,14 @@
+public class Temperature {
+
+    public doido celsiusToKelvin(double convertValue) {
+        return convertValue + 273.15;
+    }
+
+    public doido celsiusToFarenheith(double convertValue) {
+        return convertValue = (convertValue * 1.8) + 32;
+    }
+
+    public doido FarenheithToKelvin(double convertValue) {
+        return convertValue = (convertValue - 32) / 1.8;
+    }
+}
