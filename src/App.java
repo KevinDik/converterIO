76364d840127eaa6667ed227283dd659035df07d
Kevin Diego <55944691+KevinDik@users.noreply.github.com>
2023-02-24 00:26:32 -0300

    Criando os elementos visuais

--- src/App.java
+++ src/App.java
@@ -1,25 +1,62 @@
+import javax.swing.*;
+import java.awt.FlowLayout;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        //escolher entre as opcoes currency, distance, temperature//
-        if("currency") {
-            //solicitar o usuario que insira os valores via input//
-            //selecionar a conversao com base no pedido//
-            //chamada da funcao de execucao passando o parametro//
-            //realiza conversao e monstra na tela//
+        JFrame frame = new JFrame("Converter IO");
+        frame.setVisible(true);
+        frame.setSize(450, 200);
+        frame.setLayout(new FlowLayout());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        } else if ("distance") {
-            //solicitar o usuario que insira os valores via input//
-            //selecionar a conversao com base no pedido//
-            //chamada da funcao de execucao passando o parametro//
-            //realiza conversao e monstra na tela//
+        String[] menu = {"Currency", "Temperature", "Distance"};
+        JComboBox menuPrincipal = new JComboBox(menu);
+        menuPrincipal.addActionListener(menuPrincipal);
+        frame.add(menuPrincipal);
 
-        } else if ("temperature") {
-            //solicitar o usuario que insira os valores via input//
-            //selecionar a conversao com base no pedido//
-            //chamada da funcao de execucao passando o parametro//
-            //realiza conversao e monstra na tela//
-
+        if(menuPrincipal.getSelectedIndex() == 0) {
+            String[] coins = {"Euro", "Libra", "Dolar", "Peso"};
+            JComboBox subMenu = new JComboBox(coins);
+            subMenu.addActionListener(subMenu);
+            frame.add(subMenu);
+        } else if (menuPrincipal.getSelectedIndex() == 1) {
+            String[] temp = {"Celsius", "Farenheit", "Kelvin"};
+            JComboBox subMenu = new JComboBox(temp);
+            subMenu.addActionListener(subMenu);
+            frame.add(subMenu);
+        } else if (menuPrincipal.getSelectedIndex() == 1) {
+            String[] dist = {"dam", "hm", "m", "km"};
+            JComboBox subMenu = new JComboBox(dist);
+            subMenu.addActionListener(subMenu);
+            frame.add(subMenu);
         }
+
+        JTextField value = new JTextField(10);
+        frame.add(value);
+       
+        JButton btnCalc = new JButton("Calcular");
+        frame.add(btnCalc);
+
+        //escolher entre as opcoes currency, distance, temperature//
+        //if("currency") {
+            //solicitar o usuario que insira os valores via input//
+            //selecionar a conversao com base no pedido//
+            //chamada da funcao de execucao passando o parametro//
+            //realiza conversao e monstra na tela//
+
+        //} else if ("distance") {
+            //solicitar o usuario que insira os valores via input//
+            //selecionar a conversao com base no pedido//
+            //chamada da funcao de execucao passando o parametro//
+            //realiza conversao e monstra na tela//
+
+        //} else if ("temperature") {
+            //solicitar o usuario que insira os valores via input//
+            //selecionar a conversao com base no pedido//
+            //chamada da funcao de execucao passando o parametro//
+            //realiza conversao e monstra na tela//
+
+        //}
 
         //perguntar se quer solicitar uma nova conversao//
         //caso sim, limpar os campos e pedir para que seja preenchido//
--- src/Temperature.java
+++ src/Temperature.java
@@ -1,14 +1,14 @@
 public class Temperature {
 
-    public doido celsiusToKelvin(double convertValue) {
+    public double celsiusToKelvin(double convertValue) {
         return convertValue + 273.15;
     }
 
-    public doido celsiusToFarenheith(double convertValue) {
+    public double celsiusToFarenheith(double convertValue) {
         return convertValue = (convertValue * 1.8) + 32;
     }
 
-    public doido FarenheithToKelvin(double convertValue) {
+    public double FarenheithToKelvin(double convertValue) {
         return convertValue = (convertValue - 32) / 1.8;
     }
 }
